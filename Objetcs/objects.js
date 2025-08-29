d6a48d431cf423e52a28f91dbe814344a0d7edd9
Jesús Caño <j.canho@gmail.com>
2025-08-29 13:45:05 +0200

    Clase 38

--- Objetcs/objects.js
+++ Objetcs/objects.js
@@ -7,7 +7,7 @@
     // let flash: { name: string, age: number, powers: string[] } = { name: 'Barry Allen', age: 24, powers: ['Súper velocidad', 'Viajar en el tiempo'] }
     //
     //  flash = { name: 'Superman', age: 500, powers: ['Súper fuerza'] }
-    // 
+    //
     //  flash = {
     //     name: 'Superman',
     //     age: 500,
@@ -16,14 +16,20 @@
     //         return this.name
     //     }
     // }
-    let flash = { name: 'Barry Allen', age: 24, powers: ['Súper velocidad', 'Viajar en el tiempo'] };
-    flash = {
-        name: 'Superman',
-        // age: 500,
-        powers: ['Súper fuerza'],
-        // getName() {
-        //     return this.name
-        // }
-    };
-    console.log(flash);
+    //* Para evitar esto, podemos usar los atributos opcionales
+    // let flash: { name: string; age?: number; powers: string[]; getName?: () => string } = {
+    //     name: 'Barry Allen',
+    //     age: 24,
+    //     powers: ['Súper velocidad', 'Viajar en el tiempo']
+    // }
+    // 
+    // flash = {
+    //     name: 'Superman',
+    //     powers: ['Súper fuerza'],
+    //     getName() {
+    //         return this.name
+    //     }
+    // }
+    // 
+    // console.log(flash.getName?.())
 })();
--- Objetcs/objects.ts
+++ Objetcs/objects.ts
@@ -1,32 +1,35 @@
 ;(() => {
-    
     //! Pedirá los demás atributos no descritos
     //! flash = { name: 'Superman'}
-    
+
     //! Si queremos añadir atributos nuevos fallará de esta manera.
     // let flash: { name: string, age: number, powers: string[] } = { name: 'Barry Allen', age: 24, powers: ['Súper velocidad', 'Viajar en el tiempo'] }
     //
     //  flash = { name: 'Superman', age: 500, powers: ['Súper fuerza'] }
-    // 
+    //
     //  flash = {
-        //     name: 'Superman',
+    //     name: 'Superman',
     //     age: 500,
     //     powers: ['Súper fuerza'],
     //     getName() {
-        //         return this.name
-        //     }
-        // }
-        
-    let flash: { name: string, age?: number, powers: string[] } = { name: 'Barry Allen', age: 24, powers: ['Súper velocidad', 'Viajar en el tiempo'] }
+    //         return this.name
+    //     }
+    // }
 
-    flash = {
-        name: 'Superman',
-        // age: 500,
-        powers: ['Súper fuerza'],
-        // getName() {
-        //     return this.name
-        // }
-    }
-
-    console.log(flash)
+    //* Para evitar esto, podemos usar los atributos opcionales
+    // let flash: { name: string; age?: number; powers: string[]; getName?: () => string } = {
+    //     name: 'Barry Allen',
+    //     age: 24,
+    //     powers: ['Súper velocidad', 'Viajar en el tiempo']
+    // }
+    // 
+    // flash = {
+    //     name: 'Superman',
+    //     powers: ['Súper fuerza'],
+    //     getName() {
+    //         return this.name
+    //     }
+    // }
+    // 
+    // console.log(flash.getName?.())
 })()
