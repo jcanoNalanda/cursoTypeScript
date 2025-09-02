471b263c5ac7d93c6c5ea9e7819f8dbe3a2be2d0
Jesús Caño <j.canho@gmail.com>
2025-09-02 14:26:51 +0200

    Clase 60 - Constructor con private y public en typeScript

--- classes/basica.ts
+++ classes/basica.ts
@@ -1,20 +1,15 @@
 ;(() => {
     class Avenger {
-        private name: string
-
-        public team: string
-        public realName?: string
+        // private name: string
+        // public team: string
+        // public realName?: string
         static avgAge: number = 35
 
-        constructor(name: string, team: string, realName?: string) {
-            this.name = name
-            this.team = team
-            this.realName = realName
-        }
+        constructor(private name: string, private team: string, public realName?: string) {}
     }
 
-    const antman: Avenger = new Avenger('Antman', 'Team B')
+    const antman: Avenger = new Avenger('Antman', 'Team B', 'Scott Lang')
     //? console.log(Avenger.avgAge); //Las propiedades static se acceden a través de la clase
 
-    console.log(antman.team);
+    console.log(antman)
 })()
--- main.js
+++ main.js
@@ -2,14 +2,17 @@
 ;
 (() => {
     class Avenger {
+        name;
+        team;
+        realName;
+        static avgAge = 35;
         constructor(name, team, realName) {
             this.name = name;
             this.team = team;
             this.realName = realName;
         }
     }
-    Avenger.avgAge = 35;
-    const antman = new Avenger('Antman', 'Team B');
-    console.log(antman.team);
+    const antman = new Avenger('Antman', 'Team B', 'Scott Lang');
+    console.log(antman);
 })();
 //# sourceMappingURL=main.js.map
