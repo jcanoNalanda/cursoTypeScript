97a67e1b51c78bcb4c87daa9cc7aea4b89bd5f0b
Jesús Caño <j.canho@gmail.com>
2025-09-09 21:08:10 +0200

    Clase 62

--- main.js
+++ main.js
@@ -1,24 +1,27 @@
 "use strict";
-;
 (() => {
     class Avenger {
         name;
-        team;
         realName;
-        static avgAge = 35;
-        static getAvgAge() {
-            return this.name;
-        }
-        constructor(name, team, realName) {
+        constructor(name, realName) {
             this.name = name;
-            this.team = team;
             this.realName = realName;
         }
-        bio() {
-            return `${this.name} (${this.realName}) is a member of ${this.team}.`;
+        getFullName() {
+            return `${this.name} ${this.realName}`;
         }
     }
-    const antman = new Avenger('Antman', 'Team Avengers', 'Scott Lang');
-    console.log(Avenger.getAvgAge());
+    class Xmen extends Avenger {
+        isMutant;
+        constructor(name, realName, isMutant) {
+            super(name, realName);
+            this.isMutant = isMutant;
+        }
+        getFullNameDesdeXmen() {
+            return `${super.getFullName()} - ${this.isMutant ? 'Mutante' : 'No Mutante'}`;
+        }
+    }
+    const wolverine = new Xmen("Wolverine", "Logan", true);
+    console.log(wolverine.getFullNameDesdeXmen());
 })();
 //# sourceMappingURL=main.js.map
