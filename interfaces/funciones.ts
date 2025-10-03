c3079d16828dec71e4a4db7f4489f7579b8b968a
Jesús Caño <jcano@nalandaglobal.com>
2025-10-03 09:31:53 +0200

    Clase 72 - Implementación de interfaces en funciones

---
+++ interfaces/funciones.ts
@@ -0,0 +1,11 @@
+(()=>{
+    interface addTwoNumbers {
+        (a: number, b: number): number
+    }
+
+    let addNumbersFunction: addTwoNumbers
+
+    addNumbersFunction = () => {
+        return 10
+    }
+})()
