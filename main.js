97a08f19f3367ab378caed9f122a320860971720
Jesús Caño <j.canho@gmail.com>
2025-10-09 16:47:55 +0200

    Implementar funciones genéricas y métodos de impresión en el módulo de genéricos

--- main.js
+++ main.js
@@ -1,51 +1,30 @@
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-define("data/powers", ["require", "exports"], function (require, exports) {
+define("generic/generics", ["require", "exports"], function (require, exports) {
     "use strict";
     Object.defineProperty(exports, "__esModule", { value: true });
-    exports.powers = void 0;
-    exports.powers = [
-        { id: 1, desc: 'Flying' },
-        { id: 2, desc: 'Strength' }
-    ];
-    exports.default = exports.powers;
+    exports.genericFunctionArrow = exports.printObject = void 0;
+    exports.genericFunction = genericFunction;
+    const printObject = (argument) => {
+        console.log(argument);
+    };
+    exports.printObject = printObject;
+    function genericFunction(argument) {
+        return argument;
+    }
+    const genericFunctionArrow = (argument) => argument;
+    exports.genericFunctionArrow = genericFunctionArrow;
 });
-define("classes/Hero", ["require", "exports", "data/powers"], function (require, exports, powers_1) {
+define("index", ["require", "exports", "generic/generics"], function (require, exports, generics_1) {
     "use strict";
     Object.defineProperty(exports, "__esModule", { value: true });
-    exports.Hero4 = exports.Hero3 = exports.Hero2 = exports.Hero = void 0;
-    powers_1 = __importDefault(powers_1);
-    class Hero {
-        constructor(name, powerId, age) {
-            this.name = name;
-            this.powerId = powerId;
-            this.age = age;
-            this.name = name;
-            this.powerId = powerId;
-            this.age = age;
-        }
-        get power() {
-            var _a;
-            return ((_a = powers_1.default.find(power => power.id === this.powerId)) === null || _a === void 0 ? void 0 : _a.desc) || 'No tiene poder';
-        }
-    }
-    exports.Hero = Hero;
-    class Hero2 {
-    }
-    exports.Hero2 = Hero2;
-    class Hero3 {
-    }
-    exports.Hero3 = Hero3;
-    class Hero4 {
-    }
-    exports.Hero4 = Hero4;
-});
-define("index", ["require", "exports", "classes/Hero"], function (require, exports, Hero_1) {
-    "use strict";
-    Object.defineProperty(exports, "__esModule", { value: true });
-    const ironman = new Hero_1.Hero('Ironman', 100, 40);
-    console.log(ironman);
-    console.log(ironman.power);
+    (0, generics_1.printObject)(12300);
+    (0, generics_1.printObject)(new Date());
+    (0, generics_1.printObject)({ a: 1, b: 2, c: 3 });
+    (0, generics_1.printObject)('Hola a todos');
+    console.log((0, generics_1.genericFunction)(12300));
+    console.log((0, generics_1.genericFunction)({ a: 1, b: 2, c: 3 }));
+    console.log((0, generics_1.genericFunction)(new Date()).toFixed(2));
+    console.log((0, generics_1.genericFunction)('Hola a todos').toFixed(2));
+    console.log((0, generics_1.genericFunctionArrow)('Hola a todos').toFixed(2));
+    console.log((0, generics_1.genericFunctionArrow)('Hola a todos').toFixed(2));
 });
 //# sourceMappingURL=main.js.map
--- namespaces/index.ts
+++ namespaces/index.ts
@@ -1,6 +1,16 @@
-import { Hero } from './classes/Hero'; // importa varias clases 
+// Todo este código no se puede ejecutar ya que el proyecto no está configurado. Es solo para ver el código
 
- const ironman = new Hero('Ironman', 100, 40);
+import { printObject, genericFunction, genericFunctionArrow } from './generic/generics';
 
- console.log (ironman)
- console.log (ironman.power)
+printObject(12300)
+printObject(new Date())
+printObject({ a: 1, b: 2, c: 3 })
+printObject('Hola a todos')
+
+console.log(genericFunction(12300))
+console.log(genericFunction({ a: 1, b: 2, c: 3 }))
+console.log(genericFunction(new Date()).toFixed(2)) // Error porque Date no tiene el método toFixed
+console.log(genericFunction('Hola a todos').toFixed(2)) // Error porque string no tiene el método toFixed
+
+console.log(genericFunctionArrow('Hola a todos').toFixed(2)) // Error porque string no tiene el método toFixed
+console.log(genericFunctionArrow('Hola a todos').toFixed(2)) // Error porque string no tiene el método toFixed
